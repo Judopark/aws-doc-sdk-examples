5cdcf3390cd4561155342ceb90059761e3ffd5c0
JH <129403840+Judopark@users.noreply.github.com>
2024-10-04 03:03:14 +0900

    Update ResilientServiceWorkflow.cs

--- dotnetv3/cross-service/ResilientService/ResilientServiceWorkflow/ResilientServiceWorkflow.cs
+++ dotnetv3/cross-service/ResilientService/ResilientServiceWorkflow/ResilientServiceWorkflow.cs
@@ -1,4 +1,4 @@
-﻿// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+dotnetv3/cross-service/ResilientService/ResilientServiceWorkflow/ResilientServiceWorkflow.cs﻿// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
 using System.Text.Json;
